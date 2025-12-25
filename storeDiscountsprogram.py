a7712cadeed2fd79d96faf45cb92f13ec3b6d24c
Fernando <luisagamez050@gmail.com>
2025-12-25 18:12:47 -0500

    Program edition

--- storeDiscountsprogram.py
+++ storeDiscountsprogram.py
@@ -1,34 +1,34 @@
 
-print("\nPROGRAMA DE TIENDA PARA DETECTAR DESCUENTOS REALES O MAQUILLADOS\n")
+print("\nSTORE PROGRAM TO DETECT REAL OR FRAUDULENT DISCOUNTS\n")
 
 try:
-        original = float(input("Ingresa el precio original: "))
-        actual = float(input("Ingresa el precio actual mostrado: "))
-        cupon = float(input("Cupón (%): "))
+        original = float(input("Enter the original price: "))
+        actual = float(input("Enter the current displayed price: "))
+        cupon = float(input("Coupon (%): "))
 
         if original <= 0 or actual <= 0 or cupon <= 0 or cupon > 100:
-            print("Lo siento, !datos invalidos!")
+            print("Sorry, invalid data!")
         else:
             precioFinal = actual * (1 - (cupon / 100))
             descuentoReal = 1 - (precioFinal / original)
             ahorro = original - precioFinal
 
             if descuentoReal >= 0.20 and precioFinal < original:
-                print("OFERTA REAL")
-                print(f"DESCUENTO REAL: {descuentoReal * 100} %")
-                print(f"PRECIO FINAL: {precioFinal} | Ahorro: {ahorro}")
+                print("REAL OFFER")
+                print(f"REAL DISCOUNT: {descuentoReal * 100} %")
+                print(f"FINAL PRICE: {precioFinal} | Saving: {ahorro}")
 
             elif precioFinal >= original or descuentoReal < 0.08:
-                print("No es una oferta (Maquillaje o descuento trivial)")
-                print(f"DESCUENTO REAL: {descuentoReal * 100} %")
-                print(f"PRECIO FINAL: {precioFinal}")
+                print("It's not a trivial offer (makeup or discount).")
+                print(f"REAL DISCOUNT: {descuentoReal * 100} %")
+                print(f"FINAL PRICE: {precioFinal}")
 
             else:
-                print("OFERTA MODERADA")
-                print(f"DESCUENTO REAL: {descuentoReal * 100} %")
-                print(f"PRECIO FINAL: {precioFinal} | Ahorro: {ahorro}")
+                print("MODERATE OFFER")
+                print(f"REAL DISCOUNT: {descuentoReal * 100} %")
+                print(f"FINAL PRICE: {precioFinal} | Saving: {ahorro}")
 
 except ValueError:
-        print("Error: ingresa datos válidos, por favor (usa enteros o decimales).")
+        print("Error: Please enter valid data (use integers or decimals).")
 
     
